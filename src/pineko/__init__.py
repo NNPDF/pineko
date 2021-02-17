b4471556c09f9975458c35fac098b42f3f979a4f
Alessandro Candido <candido.ale@gmail.com>
2021-02-17 15:55:37 +0100

    Start loading ingredients

---
+++ src/pineko/__init__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import pathlib
+
+import yaml
+import eko
+import pineappl
+
+import yadism
+
+here = pathlib.Path(__file__).absolute().parent
+
+with open(here.parents[1] / "data" / "LO-EXA-FFNS.yaml") as f:
+    theory_card = yaml.safe_load(f)
+
+
+def generate_eko(target_filename):
+    with open(here.parents[1] / "data" / "iter10-l30m20r4.yaml") as f:
+        operators_card = yaml.safe_load(f)
+
+    ops = eko.run_dglap(theory_card=theory_card, operators_card=operators_card)
+    ops.dump_yaml_to_file(target_filename)
+
+
+def load_eko():
+    myoperator = here.parents[1] / "data" / "myoperator.yaml"
+    if not myoperator.exists():
+        generate_eko(myoperator)
+
+    ev_ops = eko.output.Output.load_yaml_from_file(myoperator)
+    return ev_ops
+
+
+def generate_yadism(target_filename):
+    with open(here.parents[1] / "data" / "observable-simple.yaml") as f:
+        observable_card = yaml.safe_load(f)
+
+    dis_cf = yadism.run_yadism(theory=theory_card, observables=observable_card)
+    dis_cf.dump_pineappl_to_file(target_filename, "F2total")
+
+
+def load_pineappl():
+    mydis = here.parents[1] / "data" / "mydis.yaml"
+    if not mydis.exists():
+        generate_eko(mydis)
+
+    # grid = pineappl.load_grid(mydis)
+    # return grid
+
+
+__import__("ipdb").set_trace()
---
+++ src/pineko/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+major = 0
+short_version = '0.0'
+version = '0.0.1'
+full_version = '0.0.1-develop'
+is_released = False
+
+__version__ = version
