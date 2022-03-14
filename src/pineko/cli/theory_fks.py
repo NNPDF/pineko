a0d58fdd12d7440ae7c73cee625bffc2229cbf10
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2022-03-14 11:54:58 +0100

    Reshuffle fks into theory

--- src/pineko/cli/theory_fks.py
+++ src/pineko/cli/theory_fks.py
@@ -1,41 +1,15 @@
 # -*- coding: utf-8 -*-
 import click
-import rich
 
-from .. import comparator, configs, evolve, parser, theory_card
+from .. import theory
 from ._base import command
 
 
 @command.command("theory_fks")
 @click.argument("theory_id", type=click.INT)
 @click.argument("datasets", type=click.STRING, nargs=-1)
-@click.option("--logs", is_flag=True, help="dump comparison")
 @click.option("--pdf", "-p", default=None, help="comparison PDF")
-def subcommand(theory_id, datasets, logs, pdf):
+@click.option("--logs", is_flag=True, help="dump comparison")
+def subcommand(theory_id, datasets, pdf, logs):
     """Compute FK tables in all datasets."""
-    # setup data
-    paths = configs.configs["paths"]
-    tcard = theory_card.load(theory_id)
-    eko_path = paths["ekos"] / str(theory_id)
-    fk_path = paths["fktables"] / str(theory_id)
-    fk_path.mkdir(exist_ok=True)
-    # iterate datasets
-    for ds in datasets:
-        rich.print(f"Analyze {ds}")
-        # iterate grids
-        grids = parser.load_grids(theory_id, ds)
-        for name, grid_path in grids.items():
-            eko_filename = eko_path / f"{name}.tar"
-            fk_filename = fk_path / f"{name}.{parser.ext}"
-            max_as = 1 + int(tcard["PTO"])
-            max_al = 0
-            # do it!
-            _grid, _fk, comparison = evolve.evolve_grid(
-                grid_path, eko_filename, fk_filename, max_as, max_al, pdf
-            )
-            # activate logging
-            if logs and paths["logs"]["fk"] and comparison:
-                logfile = paths["logs"]["fk"] / f"{theory_id}-{name}-{pdf}.log"
-                logfile.write_text(comparison.to_string())
-            rich.print(f"[green]Success:[/] Wrote FK table to {fk_filename}")
-        print()
+    theory.TheoryBuilder(theory_id, datasets).fks(pdf, logs)
--- src/pineko/theory.py
+++ src/pineko/theory.py
@@ -5,7 +5,8 @@ import eko
 import rich
 import yaml
 
-from . import configs, parser, theory_card, evolve
+from . import configs, evolve, parser, theory_card
+
 
 class TheoryBuilder:
     """Common builder application to create the ingredients for a theory.
@@ -17,6 +18,7 @@ class TheoryBuilder:
     datsets : list(str)
         list of datasets
     """
+
     def __init__(self, theory_id, datasets):
         self.theory_id = theory_id
         self.datasets = datasets
@@ -26,23 +28,86 @@ class TheoryBuilder:
         """Suffix path with theory id"""
         return configs.configs["paths"]["ekos"] / str(self.theory_id)
 
-    def opcards(self):
-        """Write operator cards."""
-        paths = configs.configs["paths"]
+    @property
+    def fk_path(self):
+        """Suffix path with theory id"""
+        return configs.configs["paths"]["fktables"] / str(self.theory_id)
+
+    def iterate(self, f, **kwargs):
+        """Iterated grids in datasets.
+
+        Additional keyword arguments are simply passed down.
+
+        Parameters
+        ----------
+        f : callable
+            iterated callable recieving name and grid as argument
+        """
         for ds in self.datasets:
             rich.print(f"Analyze {ds}")
             grids = parser.load_grids(self.theory_id, ds)
             for name, grid in grids.items():
-                opcard_path = paths["operator_cards"] / f"{name}.yaml"
-                _x_grid, q2_grid = evolve.write_operator_card_from_file(
-                    grid, paths["opcard_template"], opcard_path
-                )
-                if opcard_path.exists():
-                    rich.print(
-                        f"[green]Success:[/] Wrote card with {len(q2_grid)} Q2 points to {opcard_path}"
-                    )
+                f(name, grid, **kwargs)
             rich.print()
 
+    def opcard(self, name, grid):
+        """Write a single operator card.
+
+        Parameters
+        ----------
+        name : str
+            grid name, i.e. it's true stem
+        grid : pathlib.Path
+            path to grid
+        """
+        paths = configs.configs["paths"]
+        opcard_path = paths["operator_cards"] / f"{name}.yaml"
+        _x_grid, q2_grid = evolve.write_operator_card_from_file(
+            grid, paths["opcard_template"], opcard_path
+        )
+        if opcard_path.exists():
+            rich.print(
+                f"[green]Success:[/] Wrote card with {len(q2_grid)} Q2 points to {opcard_path}"
+            )
+
+    def opcards(self):
+        """Write operator cards."""
+        self.iterate(self.opcard)
+
+    def eko(self, name, _grid, tcard, logs):
+        """Compute a single eko.
+
+        Parameters
+        ----------
+        name : str
+            grid name, i.e. it's true stem
+        grid : pathlib.Path
+            path to grid
+        tcard : dict
+            theory card
+        logs : bool
+            save eko logs?
+        """
+        # setup data
+        paths = configs.configs["paths"]
+        opcard_path = paths["operator_cards"] / f"{name}.yaml"
+        with open(opcard_path, encoding="utf-8") as f:
+            ocard = yaml.safe_load(f)
+        eko_filename = self.eko_path / f"{name}.tar"
+        # activate logging
+        if logs and paths["logs"]["eko"]:
+            log_path = paths["logs"]["eko"] / f"{self.theory_id}-{name}.log"
+            logFile = logging.FileHandler(log_path)
+            logFile.setLevel(logging.INFO)
+            logFile.setFormatter(logging.Formatter("%(message)s"))
+            logging.getLogger("eko").handlers = []
+            logging.getLogger("eko").addHandler(logFile)
+            logging.getLogger("eko").setLevel(logging.INFO)
+        # do it!
+        ops = eko.run_dglap(theory_card=tcard, operators_card=ocard)
+        ops.dump_tar(eko_filename)
+        if eko_filename.exists():
+            rich.print(f"[green]Success:[/] Wrote EKO to {eko_filename}")
 
     def ekos(self, logs):
         """Compute all ekos.
@@ -52,32 +117,53 @@ class TheoryBuilder:
         logs : bool
             save eko logs?
         """
-        # setup data
-        paths = configs.configs["paths"]
         tcard = theory_card.load(self.theory_id)
         self.eko_path.mkdir(exist_ok=True)
-        # iterate datasets
-        for ds in self.datasets:
-            rich.print(f"Analyze {ds}")
-            # iterate grids
-            grids = parser.load_grids(self.theory_id, ds)
-            for name in grids.keys():
-                opcard_path = paths["operator_cards"] / f"{name}.yaml"
-                with open(opcard_path, encoding="utf-8") as f:
-                    ocard = yaml.safe_load(f)
-                eko_filename = self.eko_path / f"{name}.tar"
-                # activate logging
-                if logs and paths["logs"]["eko"]:
-                    log_path = paths["logs"]["eko"] / f"{self.theory_id}-{name}.log"
-                    logStdout = logging.FileHandler(log_path)
-                    logStdout.setLevel(logging.INFO)
-                    logStdout.setFormatter(logging.Formatter("%(message)s"))
-                    logging.getLogger("eko").handlers = []
-                    logging.getLogger("eko").addHandler(logStdout)
-                    logging.getLogger("eko").setLevel(logging.INFO)
-                # do it!
-                ops = eko.run_dglap(theory_card=tcard, operators_card=ocard)
-                ops.dump_tar(eko_filename)
-                if eko_filename.exists():
-                    rich.print(f"[green]Success:[/] Wrote EKO to {eko_filename}")
-            rich.print()
+        self.iterate(self.eko, tcard=tcard, logs=logs)
+
+    def fk(self, name, grid_path, tcard, pdf, logs):
+        """Compute a single FK table.
+
+        Parameters
+        ----------
+        name : str
+            grid name, i.e. it's true stem
+        grid_path : pathlib.Path
+            path to grid
+        tcard : dict
+            theory card
+        pdf : str
+            comparison PDF
+        logs : bool
+            save eko logs?
+        """
+        # setup data
+        paths = configs.configs["paths"]
+        eko_filename = self.eko_path / f"{name}.tar"
+        fk_filename = self.fk_path / f"{name}.{parser.ext}"
+        max_as = 1 + int(tcard["PTO"])
+        max_al = 0
+        # do it!
+        _grid, _fk, comparison = evolve.evolve_grid(
+            grid_path, eko_filename, fk_filename, max_as, max_al, pdf
+        )
+        # activate logging
+        if logs and paths["logs"]["fk"] and comparison:
+            logfile = paths["logs"]["fk"] / f"{self.theory_id}-{name}-{pdf}.log"
+            logfile.write_text(comparison.to_string())
+        if fk_filename.exists():
+            rich.print(f"[green]Success:[/] Wrote FK table to {fk_filename}")
+
+    def fks(self, pdf, logs):
+        """Compute all FK tables.
+
+        Parameters
+        ----------
+        pdf : str
+            comparison PDF
+        logs : bool
+            save eko logs?
+        """
+        tcard = theory_card.load(self.theory_id)
+        self.fk_path.mkdir(exist_ok=True)
+        self.iterate(self.fk, tcard=tcard, pdf=pdf, logs=logs)
