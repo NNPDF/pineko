918f547d80c4bf81f86f7d66fcd4241dd1a8ddf8
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2022-06-08 16:59:15 +0200

    Init pydocstyle and update pre-commit

--- data/grids/load.sh
+++ data/grids/load.sh
@@ -6,4 +6,4 @@
 # theory example
 # scp dom.mi.infn.it:/media/FK/fktables/data/grids/200-SLAC_NC_EM_D_F2.pineappl.lz4 ./200/SLAC_NC_EM_D_F2.pineappl.lz4
 # scp dom.mi.infn.it:/media/FK/fktables/data/grids/200-SLAC_NC_EM_P_F2.pineappl.lz4 ./200/SLAC_NC_EM_P_F2.pineappl.lz4
-# scp dom.mi.infn.it:/media/FK/fktables_pineko/data/grids/208/HERA_NC_318GEV_EAVG_SIGMARED_BOTTOM.pineappl.lz4 ./208/HERA_NC_318GEV_EAVG_SIGMARED_BOTTOM.pineappl.lz4
+# scp dom.mi.infn.it:/media/FK/fktables_pineko/data/grids/208/HERA_CC_318GEV_EM_SIGMARED.pineappl.lz4 ./208/HERA_CC_318GEV_EM_SIGMARED.pineappl.lz4
