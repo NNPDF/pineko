c076e79d4a057629911fcbde61318c0777452779
andreab1997 <andreab1997@yahoo.it>
2022-06-13 15:13:35 +0200

    Fixed init of cli

--- src/pineko/cli/__init__.py
+++ src/pineko/cli/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-from . import check, compare, convolute, opcard, theory_
+from . import check, check_scalevar, compare, convolute, opcard, theory_
 from ._base import command
