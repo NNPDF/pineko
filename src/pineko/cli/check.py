cfcdf117d2c7729eb6347bb622635f76208a43b3
andreab1997 <andreab1997@yahoo.it>
2022-06-13 14:39:53 +0200

    Fixing

--- src/pineko/cli/check.py
+++ src/pineko/cli/check.py
@@ -28,13 +28,3 @@ def subcommand(pineappl_path, eko_path, xif):
         rich.print("[green]Success:[/] grids are compatible")
     except ValueError as e:
         rich.print("[red]Error:[/]", e)
-
-
-@command.command("check_scalevar")
-@click.argument("pineappl_path", metavar="PINEAPPL", type=click.Path(exists=True))
-@click.argument("theory_ID", metavar="ID", type=click.Path(exists=True))
-def subcommand(pineappl_path, theory_ID):
-    """Check if PineAPPL grid contains scale variations if theory card needs them."""
-    theory_card = theory_card.load(theory_ID)
-    pineappl_grid = pineappl.grid.Grid.read(pineappl_path)
-    check.check_grid_contains_sv(pineappl_grid, theory_card)
