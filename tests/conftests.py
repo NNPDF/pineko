0eca8f520e02edd878c23bfa64f2c29648ac7ee1
andreab1997 <andreab1997@yahoo.it>
2022-06-15 16:10:17 +0200

    Init test theory

--- tests/conftests.py
+++ tests/conftests.py
@@ -19,3 +19,19 @@ def cd():
             os.chdir(prevdir)
 
     return wrapped
+
+
+@pytest.fixture
+def lhapdf_path():
+    @contextmanager
+    def wrapped(newdir):
+        import lhapdf  # pylint: disable=import-error, import-outside-toplevel
+
+        paths = lhapdf.paths()
+        lhapdf.pathsPrepend(str(newdir))
+        try:
+            yield
+        finally:
+            lhapdf.setPaths(paths)
+
+    return wrapped
---
+++ tests/test_theory.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import pathlib
+
+import pineko
+
+theory_obj = pineko.theory.TheoryBuilder(208, ["HERACOMBCCEM"])
+test_files = pathlib.Path(__file__).parents[0] / "test_files/"
+
+
+def test_operators_cards_path():
+    path = theory_obj.operator_cards_path
+    assert path == pathlib.Path(test_files / "data/operator_cards/208")
+
+
+def test_ekos_path():
+    path = theory_obj.ekos_path()
+    assert path == pathlib.Path(test_files / "data/ekos/208")
+
+
+def test_fks_path():
+    path = theory_obj.fks_path
+    assert path == pathlib.Path(test_files / "data/fktables/208")
+
+
+def test_grids_path():
+    path = theory_obj.grids_path()
+    assert path == pathlib.Path(test_files / "data/grids/208")
+
+
+def test_load_grids():
+    dataset_name = "HERACOMBCCEM"
+    grids = theory_obj.load_grids(dataset_name)
+    assert grids["HERA_CC_318GEV_EM_SIGMARED"] == pathlib.Path(
+        test_files / "data/grids/208/HERA_CC_318GEV_EM_SIGMARED.pineappl.lz4"
+    )
