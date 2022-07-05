72c7889606bd8181c79e01af41415b7691a3b59d
andreab1997 <andreab1997@yahoo.it>
2022-07-05 12:02:26 +0200

    Change argument of the function and put tocheck as argument

--- src/pineko/theory.py
+++ src/pineko/theory.py
@@ -350,10 +350,11 @@ class TheoryBuilder:
         xir = tcard["XIR"]
         xif = tcard["XIF"]
         ftr = tcard["fact_to_ren_scale_ratio"]
+        pineappl_grid = pineappl.grid.Grid.read(grid_path)
         if not np.isclose(xir, 1.0):
-            check.check_grid_contains_ren_sv(grid_path)
+            check.check_grid_contains_ren_sv(pineappl_grid)
         if not (np.isclose(xif, 1.0) and np.isclose(ftr, 1.0)):
-            check.check_grid_contains_fact_sv(grid_path)
+            check.check_grid_contains_fact_sv(pineappl_grid)
         # setup data
         eko_filename = self.ekos_path() / f"{name}.tar"
         fk_filename = self.fks_path / f"{name}.{parser.EXT}"
