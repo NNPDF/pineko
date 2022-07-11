557a5fb4a209304f233d6879878739edab04e0b4
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2022-07-11 11:32:10 +0200

    Branch debug stuff

--- debug.py
+++ debug.py
@@ -12,6 +12,7 @@ hpdf = apply.apply_pdf(ho, gonly)
 bo = eko.output.Output.load_tar("data/ekos/2201/test.tar")
 bpdf = apply.apply_pdf(bo, gonly)
 
+# a is the output of the sv script
 plot(np.log(a[:, 2]), a[:, 4])
 plot(np.log(a[:, 2]), a[:, 6])
 plot(np.log(a[:, 2]), 4 * 5 / 9 * a[:, 2] * bpdf[300.0]["pdfs"][2])
