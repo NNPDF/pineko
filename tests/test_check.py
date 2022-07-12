697d987eb3949543bae121dacc881d72f546a835
andreab1997 <andreab1997@yahoo.it>
2022-07-12 13:35:54 +0200

    Added relevant test

--- tests/test_check.py
+++ tests/test_check.py
@@ -33,3 +33,13 @@ def test_check_grid_and_eko_compatible():
     with pytest.raises(ValueError):
         pineko.check.check_grid_and_eko_compatible(wrong_grid, ekoop)
     pineko.check.check_grid_and_eko_compatible(grid, ekoop)
+
+def test_is_fonll_b():
+    fns = "FONLL-B"
+    lumi = [[(1, 11, 3, 4), (3, 11, 5, 6)], [(9, 11, 0, 3), (8, 11, -2, -1)]]
+    assert pineko.check.is_fonll_b(fns, lumi) is True
+    lumi.append([(1, 11, 2, 3), (2, 4, 5, 6)])
+    assert pineko.check.is_fonll_b(fns, lumi) is False
+    lumi.pop(-1)
+    fns = "FONLL-C"
+    assert pineko.check.is_fonll_b(fns, lumi) is False
