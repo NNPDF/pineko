f45dcf8b812aed1d58c6257d8797fadbfa2e640a
andreab1997 <andreab1997@yahoo.it>
2022-08-18 14:11:37 +0200

    Fixed integrability opcards

--- src/pineko/cli/opcard.py
+++ src/pineko/cli/opcard.py
@@ -14,16 +14,19 @@ from ._base import command
 )
 @click.argument("opcard_path", metavar="OPCARD", type=click.Path())
 @click.option("--xif", default=1.0, help="factorization scale variation")
-def subcommand(pineappl_path, default_card_path, opcard_path, xif):
+@click.option("--int", default=False, type=bool, help="is integrability?")
+def subcommand(pineappl_path, default_card_path, opcard_path, xif, int):
     """Write EKO card for PineAPPL grid.
 
     Writes a copy of the default card from DEFAULT_CARD to OPCARD
     with the adjusted x grid and Q2 grid read from PINEAPPL.
 
     XIF is the factorization scale variation.
+
+    If the grid is an integrability grid, INT must be true.
     """
     _x_grid, q2_grid = evolve.write_operator_card_from_file(
-        pineappl_path, default_card_path, opcard_path, xif
+        pineappl_path, default_card_path, opcard_path, xif, int
     )
     rich.print(
         f"[green]Success:[/] Wrote card with {len(q2_grid)} Q2 points to {opcard_path}"
