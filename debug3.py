df5c4b60fcf7529f8e64589bc8b02040234294a5
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2022-10-06 17:53:37 +0200

    Start NNLO debug

--- debug3.py
+++ debug3.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from enum import Enum
 import sys
 import lhapdf
 import numpy as np
@@ -11,6 +12,7 @@ from eko import interpolation
 from eko import output
 from eko import strong_coupling as sc
 from ekomark.apply import apply_pdf
+from eko.beta import beta_0
 
 # from eko.output.struct import EKO
 from yadism.coefficient_functions.splitting_functions import lo, nlo
@@ -19,15 +21,26 @@ from yadism.esf.conv import convolute_operator
 # mode can be gluon only or charm only
 mode_g = True
 if len(sys.argv) > 1:
-    fl = sys.argv[1].strip()
+    fl = sys.argv[1].strip().lower()
     if fl == "g":
         mode_g = True
     if fl == "c":
         mode_g = False
 
-mode_no_nlo = True
+# order mode
+class Order(Enum):
+    nonlo = 0
+    nlo = 1
+    nnlo = 2
+
+
+order_mode = Order.nonlo
 if len(sys.argv) > 2:
-    mode_no_nlo = sys.argv[1].strip().lower() == "nlo"
+    om = sys.argv[1].strip().lower()
+    if om == "nlo":
+        order_mode = Order.nlo
+    elif om == "nnlo":
+        order_mode = Order.nnlo
 
 # load respective PDF set
 if mode_g:
@@ -36,19 +49,23 @@ else:
     pdf = lhapdf.mkPDF("conly", 0)
 
 # load FK tables
-if mode_no_nlo:
+if order_mode == Order.nonlo:
     b1 = pineappl.fk_table.FkTable.read("data/fktables/2205/no-nlo-test.pineappl.lz4")
     c1 = pineappl.fk_table.FkTable.read("data/fktables/3205/no-nlo-test.pineappl.lz4")
-else:
+elif order_mode == Order.nlo:
     b1 = pineappl.fk_table.FkTable.read("data/fktables/2205/test.pineappl.lz4")
     c1 = pineappl.fk_table.FkTable.read("data/fktables/3205/test.pineappl.lz4")
+elif order_mode == Order.nnlo:
+    b1 = pineappl.fk_table.FkTable.read("data/fktables/2205/test-nnlo.pineappl.lz4")
+    c1 = pineappl.fk_table.FkTable.read("data/fktables/3205/test-nnlo.pineappl.lz4")
 out = yadism.output.Output.load_tar("test0.tar")
 
 # compute predictions from FK
 bb1 = b1.convolute_with_one(2212, pdf.xfxQ2)
 cc1 = c1.convolute_with_one(2212, pdf.xfxQ2)
 
-# load ingredients from yadism
+# load ingredients from yadism ---
+# LO
 loc = np.array(
     [
         out["F2_light"][j].orders[(0, 0, 0, 0)][0][-3]
@@ -61,6 +78,7 @@ los = np.array(
         for j in range(len(out["interpolation_xgrid"]))
     ]
 )
+# NLO
 nloc = np.array(
     [
         out["F2_light"][j].orders[(1, 0, 0, 0)][0][-3]
@@ -79,25 +97,73 @@ nlog = np.array(
         for j in range(len(out["interpolation_xgrid"]))
     ]
 )
-pqqc = np.array(
+# NLO SV
+nlosvc = np.array(
     [
         out["F2_light"][j].orders[(1, 0, 0, 1)][0][-3]
         for j in range(len(out["interpolation_xgrid"]))
     ]
 )
-pqgg = np.array(
+nlosvg = np.array(
     [
         out["F2_light"][j].orders[(1, 0, 0, 1)][0][7]
+        for j in range(len(out["interpolation_xgrid"]))
+    ]
+)
+# NNLO
+nnloc = np.array(
+    [
+        out["F2_light"][j].orders[(2, 0, 0, 0)][0][-3]
+        for j in range(len(out["interpolation_xgrid"]))
+    ]
+)
+nnlos = np.array(
+    [
+        out["F2_light"][j].orders[(2, 0, 0, 0)][0][-3]
+        for j in range(len(out["interpolation_xgrid"]))
+    ]
+)
+nnlog = np.array(
+    [
+        out["F2_light"][j].orders[(2, 0, 0, 0)][0][7]
+        for j in range(len(out["interpolation_xgrid"]))
+    ]
+)
+# NNLO SV
+nnlosv1c = np.array(
+    [
+        out["F2_light"][j].orders[(2, 0, 0, 1)][0][-3]
+        for j in range(len(out["interpolation_xgrid"]))
+    ]
+)
+nnlosv1g = np.array(
+    [
+        out["F2_light"][j].orders[(2, 0, 0, 1)][0][7]
+        for j in range(len(out["interpolation_xgrid"]))
+    ]
+)
+nnlosv2c = np.array(
+    [
+        out["F2_light"][j].orders[(2, 0, 0, 2)][0][-3]
+        for j in range(len(out["interpolation_xgrid"]))
+    ]
+)
+nnlosv2g = np.array(
+    [
+        out["F2_light"][j].orders[(2, 0, 0, 2)][0][7]
         for j in range(len(out["interpolation_xgrid"]))
     ]
 )
 
 # compute splitting functions
 interp = interpolation.InterpolatorDispatcher.from_dict(out, mode_N=False)
-pqq = convolute_operator(lo.pqq(5), interp)[0]
-pqg = convolute_operator(lo.pqg(5), interp)[0] / 10  # * 55 / 100
-pgq = convolute_operator(nlo.pgq0(5), interp)[0]
-pgg = convolute_operator(nlo.pgg0(5), interp)[0]
+pqq0 = convolute_operator(lo.pqq(5), interp)[0].T
+pqg0 = convolute_operator(lo.pqg(5), interp)[0].T / 10  # * 55 / 100
+pgq0 = convolute_operator(nlo.pgq0(5), interp)[0].T
+pgg0 = convolute_operator(nlo.pgg0(5), interp)[0].T
+pqq1 = convolute_operator(nlo.pqq1(5), interp)[0].T
+pqg1 = convolute_operator(nlo.pqg1(5), interp)[0].T
+beta0I = beta_0(5) * np.eye(pqg0.shape[0])
 
 q2 = 300.0
 muf2 = q2 * 2.0**2
@@ -111,20 +177,25 @@ ab = astrong.a_s(q2)
 L = np.log(1.0 / 2.0**2)
 
 # shut yadism references down
-if mode_no_nlo:
+if order_mode == Order.nonlo:
     nlog *= 0
     nloc *= 0
     nlos *= 0
+if order_mode == Order.nonlo or order_mode == Order.nlo:
+    nnlog *= 0
+    nnloc *= 0
+    nnlos *= 0
 
 # create joint quark terms
 loq = 2 * (3 * los + 2 * loc)
 nloq = 2 * (3 * nlos + 2 * nloc)
+nnloq = 2 * (3 * nnlos + 2 * nnloc)
 
 # construct K
-Kqq = np.eye(len(out["interpolation_xgrid"])) + ac * L * pqq.T
-Kgg = np.eye(len(out["interpolation_xgrid"])) + ac * L * pgg.T
-Kqg = ac * L * pqg.T
-Kgq = ac * L * pgq.T
+Kqq = np.eye(len(out["interpolation_xgrid"])) + ac * L * pqq0
+Kgg = np.eye(len(out["interpolation_xgrid"])) + ac * L * pgg0
+Kqg = ac * L * pqg0
+Kgq = ac * L * pgq0
 
 # compute PDFs
 if mode_g:
@@ -132,19 +203,64 @@ if mode_g:
 else:
     f = np.array([pdf.xfxQ2(4, x, muf2) / x for x in out["interpolation_xgrid"]])
 
-# check yadism SV is proportional splitting functions
-# print("check yadism SV")
-# if mode_g:
-#     print((pqgg @ f)/(loq @ pqg.T @ f))
-# else:
-#     print((pqqc @ f)/(loc @ pqq.T @ f))
+
+def check_yad_sv(plot=False):
+    """Check if yadism SV is proportional to splitting functions."""
+    print("check yadism SV")
+    # collect data
+    ops = {
+        "nlosv": None,
+        "nnlosv1": None,
+        "nnlosv2": None,
+    }
+    if mode_g:
+        ops["nlosv"] = (nlosvg, loq @ pqg0)
+        ops["nnlosv1"] = (
+            nnlosv1g,
+            loq @ pqg1 + nlog @ (pgg0 - beta0I) + nloq @ pqg0,
+        )
+        ops["nnlosv2"] = (
+            nnlosv2g,
+            1.0 / 2.0 * loq @ pqq0 @ pqg0 + 1.0 / 2.0 * (loq @ pqg0 @ (pgg0 - beta0I)),
+        )
+    else:
+        ops["nlosv"] = (nlosvc, loc @ pqq0)
+        ops["nnlosv1"] = (
+            nnlosv1c,
+            loc @ pqq1 + nloc @ (pqq0 - beta0I) + nlog @ pgq0,
+        )
+        ops["nnlosv2"] = (
+            nnlosv2c,
+            1.0 / 2.0 * loq @ pqg0 @ pgq0 + 1.0 / 2.0 * (loc @ pqq0 @ (pqq0 - beta0I)),
+        )
+    # print check
+    for k, v in ops.items():
+        if v is None:
+            continue
+        print("ratio", k)
+        print((v[0] @ f) / (v[1] @ f))
+        # show actual curves?
+        if plot:
+            plt.title(f"{k}@{pdf.set().name}")
+            plt.plot((v[0] @ f), label="yad")
+            plt.plot((v[1] @ f), label="ana")
+            plt.legend()
+            plt.show()
+
+
+check_yad_sv(True)
 
 # extract a by comparing C against yadism
-# print("check C result")
-# if mode_g:
-#     print(cc1/((ac*(nlog + pqgg*L)) @ f))
-# else:
-#     print(cc1/((loc + ac*(nloc + pqqc*L)) @ f))
+def check_c_res():
+    print("check C result")
+    if mode_g:
+        op = ac * (nlog + nlosvg * L)
+    else:
+        op = loc + ac * (nloc + nlosvc * L)
+    print(cc1 / (op @ f))
+
+
+# check_c_res()
 
 # check B EKO is K
 # print("check B EKO result")
@@ -181,26 +297,25 @@ else:
 # plt.show()
 
 # check difference between B and C
-print("check B - C")
-diff_grid = bb1 - cc1
-# plt.plot(bb1, label="b-grid")
-# plt.plot(cc1, label="c-grid")
-# nloc *= 1.9
-# nlog *= 1.14
-if mode_g:
-    # plt.plot(a**2 * L * ((nloc @ pqg.T @ f)), label="pqg")
-    # plt.plot(a**2 * L * ((nlog @ pgg.T @ f)), label="pgg")
-    diff_ana = (nlog * (ab - ac) + ac * ab * L * ((nloq @ pqg.T) + (nlog @ pgg.T))) @ f
-else:
-    # plt.plot(a**2 * L * ((nloc @ pqq.T @ f)), label="pqq")
-    # plt.plot(a**2 * L * ((nlog @ pgq.T @ f)), label="pgq")
-    diff_ana = (nloc * (ab - ac) + ab * ac * L * ((nloc @ pqq.T) + (nlog @ pgq.T))) @ f
-# print(diff_grid/cc1)
-print(bb1 / cc1)
-print(diff_grid / diff_ana)
-plt.plot(diff_grid, label="grid")
-plt.plot(diff_ana, label="ana")
-# plt.plot((diff_grid/bb1-1)*100, label="rel err. diff to b")
-# plt.plot(diff_grid/diff_ana, label="ratio")
-plt.legend()
-plt.show()
+# print("check B - C")
+# if order_mode == Order.nlo or order_mode == Order.nnlo:
+#     diff_grid = bb1 - cc1
+#     # plt.plot(bb1, label="b-grid")
+#     # plt.plot(cc1, label="c-grid")
+#     if mode_g:
+#         # plt.plot(a**2 * L * ((nloc @ pqg0 @ f)), label="pqg0")
+#         # plt.plot(a**2 * L * ((nlog @ pgg0 @ f)), label="pgg0")
+#         diff_ana = (nlog * (ab - ac) + ac * ab * L * ((nloq @ pqg0) + (nlog @ pgg0))) @ f
+#     else:
+#         # plt.plot(a**2 * L * ((nloc @ pqq0 @ f)), label="pqq0")
+#         # plt.plot(a**2 * L * ((nlog @ pgq0 @ f)), label="pgq0")
+#         diff_ana = (nloc * (ab - ac) + ab * ac * L * ((nloc @ pqq0) + (nlog @ pgq0))) @ f
+#     # print(diff_grid/cc1)
+#     print(bb1 / cc1)
+#     print(diff_grid / diff_ana)
+#     # plt.plot(diff_grid, label="grid")
+#     # plt.plot(diff_ana, label="ana")
+#     # plt.plot((diff_grid/bb1-1)*100, label="rel err. diff to b")
+#     # plt.plot(diff_grid/diff_ana, label="ratio")
+#     # plt.legend()
+#     # plt.show()
