559ed8a906cf160b6139e5aab5c969a906640642
andreab1997 <andreab1997@yahoo.it>
2022-10-20 10:35:07 +0200

    Separate script and remove lhapdf.conf

---
+++ .github/workflows/download_test_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -r -np -nH --cut-dirs=1 -l 4  -e robots=off --no-verbose -P benchmarks -R index.* https://data.nnpdf.science/pineko/data_files/
+wget -r -np -nH --cut-dirs=1 -l 4  -e robots=off --no-verbose -P benchmarks -R index.* https://data.nnpdf.science/pineko/fakepdfs/
