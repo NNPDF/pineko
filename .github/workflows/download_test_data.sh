8680c24501ee9dba55906b9a3a1171e0c5bf306a
andreab1997 <andreab1997@yahoo.it>
2022-10-20 10:43:37 +0200

    Temporary test

--- .github/workflows/download_test_data.sh
+++ .github/workflows/download_test_data.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+apt-get install wget
 wget -r -np -nH --cut-dirs=1 -l 4  -e robots=off --no-verbose -P benchmarks -R index.* https://data.nnpdf.science/pineko/data_files/
 wget -r -np -nH --cut-dirs=1 -l 4  -e robots=off --no-verbose -P benchmarks -R index.* https://data.nnpdf.science/pineko/fakepdfs/
