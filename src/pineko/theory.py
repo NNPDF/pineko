98911439730ba9c4b34f7e64311e9d83b32f928c
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2022-12-06 15:29:43 +0100

    Scope op card template in theory

--- src/pineko/theory.py
+++ src/pineko/theory.py
@@ -221,8 +221,7 @@ class TheoryBuilder:
                 return
         _x_grid, q2_grid = evolve.write_operator_card_from_file(
             grid,
-            self.operator_cards_path
-            / configs.configs["paths"]["operator_card_template_name"],
+            self.operator_cards_path / configs.configs["paths"]["operator_card_template_name"],
             opcard_path,
             tcard,
         )
