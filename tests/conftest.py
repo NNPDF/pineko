0606251a533163d6c56a27eec311dc6ed3d4e9e6
andreab1997 <andreab1997@yahoo.it>
2022-12-21 17:13:17 +0100

    Make fake_configs fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,48 @@
+import pytest
+
+
+@pytest.fixture
+def my_tmp_path(tmp_path):
+    return tmp_path
+
+
+@pytest.fixture
+def construct_wrong_fake_configs(my_tmp_path):
+    wrong_fake_configs = {
+        "paths": {
+            "ymldb": my_tmp_path / "data/ymldb",
+            "logs": {"eko": my_tmp_path / "logs/eko", "fk": ["something", "wrong"]},
+        },
+        "root": my_tmp_path,
+    }
+    return wrong_fake_configs
+
+
+@pytest.fixture
+def construct_fake_configs_incomplete(my_tmp_path):
+    fake_configs_incomplete = {
+        "paths": {
+            "ymldb": my_tmp_path / "data/ymldb",
+            "logs": {"eko": my_tmp_path / "logs/eko"},
+        },
+        "root": my_tmp_path,
+    }
+    return fake_configs_incomplete
+
+
+@pytest.fixture
+def construct_fake_configs(my_tmp_path):
+    fake_configs = {
+        "paths": {
+            "ymldb": my_tmp_path / "data/ymldb",
+            "operator_cards": my_tmp_path / "data/operator_cards",
+            "grids": my_tmp_path / "data/grids",
+            "operator_card_template_name": "_template.yaml",
+            "theory_cards": my_tmp_path / "data/theory_cards",
+            "fktables": my_tmp_path / "data/fktables",
+            "ekos": my_tmp_path / "data/ekos",
+            "logs": {"eko": my_tmp_path / "logs/eko"},
+        },
+        "root": my_tmp_path,
+    }
+    return fake_configs
--- tests/test_scaffold.py
+++ tests/test_scaffold.py
@@ -5,58 +5,21 @@ import pytest
 from pineko import configs, scaffold
 
 
-def construct_wrong_fake_configs(tmp_path):
-    wrong_fake_configs = {
-        "paths": {
-            "ymldb": tmp_path / "data/ymldb",
-            "logs": {"eko": tmp_path / "logs/eko", "fk": ["something", "wrong"]},
-        },
-        "root": tmp_path,
-    }
-    return wrong_fake_configs
-
-
-def construct_fake_configs_incomplete(tmp_path):
-    fake_configs_incomplete = {
-        "paths": {
-            "ymldb": tmp_path / "data/ymldb",
-            "logs": {"eko": tmp_path / "logs/eko"},
-        },
-        "root": tmp_path,
-    }
-    return fake_configs_incomplete
-
-
-def construct_fake_configs(tmp_path):
-    fake_configs = {
-        "paths": {
-            "ymldb": tmp_path / "data/ymldb",
-            "operator_cards": tmp_path / "data/operator_cards",
-            "grids": tmp_path / "data/grids",
-            "operator_card_template_name": "_template.yaml",
-            "theory_cards": tmp_path / "data/theory_cards",
-            "fktables": tmp_path / "data/fktables",
-            "ekos": tmp_path / "data/ekos",
-            "logs": {"eko": tmp_path / "logs/eko"},
-        },
-        "root": tmp_path,
-    }
-    return fake_configs
-
-
-def test_set_up_project(tmp_path):
-    wrong_fake_configs = construct_wrong_fake_configs(tmp_path)
+def test_set_up_project(
+    my_tmp_path, construct_wrong_fake_configs, construct_fake_configs_incomplete
+):
+    wrong_fake_configs = construct_wrong_fake_configs
     with pytest.raises(TypeError):
         scaffold.set_up_project(wrong_fake_configs)
-    fake_configs_incomplete = construct_fake_configs_incomplete(tmp_path)
+    fake_configs_incomplete = construct_fake_configs_incomplete
     scaffold.set_up_project(fake_configs_incomplete)
-    assert (tmp_path / "data/ymldb").exists()
-    assert (tmp_path / "logs/eko").exists()
+    assert (my_tmp_path / "data/ymldb").exists()
+    assert (my_tmp_path / "logs/eko").exists()
 
 
-def test_check_folder(tmp_path):
+def test_check_folder(construct_fake_configs_incomplete, construct_fake_configs):
     # we may fail because we use a wrong config ...
-    fake_configs_incomplete = construct_fake_configs_incomplete(tmp_path)
+    fake_configs_incomplete = construct_fake_configs_incomplete
     scaffold.set_up_project(fake_configs_incomplete)
     success, wrong_confs, _wrong_folders = scaffold.check_folders(
         fake_configs_incomplete
@@ -71,7 +34,7 @@ def test_check_folder(tmp_path):
         "ekos",
     ] == wrong_confs
     # or because we didn't setup up properly ...
-    fake_configs = construct_fake_configs(tmp_path)
+    fake_configs = construct_fake_configs
     success, wrong_confs, wrong_folders = scaffold.check_folders(fake_configs)
     assert success == False
     assert len(wrong_confs) == 0
