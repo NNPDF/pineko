51ba8e33333f006f46027b2f36da571758d30cad
andreab1997 <andreab1997@yahoo.it>
2023-01-03 15:33:38 +0100

    Remove slash from paths

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,8 +11,8 @@ def construct_wrong_fake_configs(my_tmp_path):
     """This configs are wrong because under logs/fk there is a list and not a string."""
     wrong_fake_configs = {
         "paths": {
-            "ymldb": my_tmp_path / "data/ymldb",
-            "logs": {"eko": my_tmp_path / "logs/eko", "fk": ["something", "wrong"]},
+            "ymldb": my_tmp_path / "data" / "ymldb",
+            "logs": {"eko": my_tmp_path / "logs" / "eko", "fk": ["something", "wrong"]},
         },
         "root": my_tmp_path,
     }
@@ -24,8 +24,8 @@ def construct_fake_configs_incomplete(my_tmp_path):
     "This configs are incomplete because we are missing for instance the ekos and fktables keys."
     fake_configs_incomplete = {
         "paths": {
-            "ymldb": my_tmp_path / "data/ymldb",
-            "logs": {"eko": my_tmp_path / "logs/eko"},
+            "ymldb": my_tmp_path / "data" / "ymldb",
+            "logs": {"eko": my_tmp_path / "logs" / "eko"},
         },
         "root": my_tmp_path,
     }
@@ -36,14 +36,14 @@ def construct_fake_configs_incomplete(my_tmp_path):
 def construct_fake_configs(my_tmp_path):
     fake_configs = {
         "paths": {
-            "ymldb": my_tmp_path / "data/ymldb",
-            "operator_cards": my_tmp_path / "data/operator_cards",
-            "grids": my_tmp_path / "data/grids",
+            "ymldb": my_tmp_path / "data" / "ymldb",
+            "operator_cards": my_tmp_path / "data" / "operator_cards",
+            "grids": my_tmp_path / "data" / "grids",
             "operator_card_template_name": "_template.yaml",
-            "theory_cards": my_tmp_path / "data/theory_cards",
-            "fktables": my_tmp_path / "data/fktables",
-            "ekos": my_tmp_path / "data/ekos",
-            "logs": {"eko": my_tmp_path / "logs/eko"},
+            "theory_cards": my_tmp_path / "data" / "theory_cards",
+            "fktables": my_tmp_path / "data" / "fktables",
+            "ekos": my_tmp_path / "data" / "ekos",
+            "logs": {"eko": my_tmp_path / "logs" / "eko"},
         },
         "root": my_tmp_path,
     }
