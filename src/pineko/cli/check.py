dd87f918d85d3d2dd8ebf5a8bf6164bf84636a76
andreab1997 <andreab1997@yahoo.it>
2023-01-13 13:08:07 +0100

    Changing all the eko.output import in cli

--- src/pineko/cli/check.py
+++ src/pineko/cli/check.py
@@ -30,7 +30,7 @@ def sub_compatibility(grid_path, operator_path, xif):
 
     """
     pineappl_grid = pineappl.grid.Grid.read(grid_path)
-    operators = eko.output.legacy.load_tar(operator_path)
+    operators = eko.io.legacy.load_tar(operator_path)
     try:
         check.check_grid_and_eko_compatible(pineappl_grid, operators, xif)
         rich.print("[green]Success:[/] grids are compatible")
