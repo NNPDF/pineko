07b097f505641d99b73ce08c3e5b3a1ceedc73ce
andreab1997 <andreab1997@yahoo.it>
2023-01-17 17:52:08 +0100

    Fix bench_checks

--- benchmarks/bench_checks.py
+++ benchmarks/bench_checks.py
@@ -11,7 +11,7 @@ import pineko.check
 def benchmark_check_grid_and_eko_compatible(test_files, tmp_path):
     oppath = tmp_path / "eko.tar"
     grid = pineappl.grid.Grid.read(
-        test_files / "data/grids/208/HERA_CC_318GEV_EM_SIGMARED.pineappl.lz4"
+        test_files / "data/grids/400/HERA_NC_225GEV_EP_SIGMARED.pineappl.lz4"
     )
     wrong_grid = pineappl.grid.Grid.read(
         test_files / "data/grids/208/NUTEV_CC_NU_FE_SIGMARED.pineappl.lz4"
@@ -20,7 +20,9 @@ def benchmark_check_grid_and_eko_compatible(test_files, tmp_path):
         eko.io.legacy.load_tar(
             test_files / "data/ekos/208/HERA_CC_318GEV_EM_SIGMARED.tar", oppath
         )
-    with eko.EKO.edit(oppath) as ekoop:
+    with eko.EKO.edit(
+        test_files / "data/ekos/400/HERA_NC_225GEV_EP_SIGMARED.tar"
+    ) as ekoop:
         with pytest.raises(ValueError):
             pineko.check.check_grid_and_eko_compatible(wrong_grid, ekoop, 1.0)
         pineko.check.check_grid_and_eko_compatible(grid, ekoop, 1.0)
