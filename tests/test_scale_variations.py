Fix signs also in tests

tests/test_scale_variations.py
@@ -17,7 +17,7 @@ def test_ren_sv_coeffs():
     np.testing.assert_allclose(
         res_nf5 / res_nf4, beta_qcd((2, 0), 5) / beta_qcd((2, 0), 4)
     )
-    exp_res = -beta_qcd((2, 0), 5) * (1.0 / (4.0 * np.pi))
+    exp_res = beta_qcd((2, 0), 5) * (1.0 / (4.0 * np.pi))
     np.testing.assert_allclose(
         scale_variations.ren_sv_coeffs(m=0, max_as=2, logpart=1, which_part=1, nf=5),
         exp_res,
@@ -25,7 +25,7 @@ def test_ren_sv_coeffs():
 
 
 def test_compute_scale_factor():
-    exp_res = -beta_qcd((2, 0), 5) * (1.0 / (4.0 * np.pi))
+    exp_res = beta_qcd((2, 0), 5) * (1.0 / (4.0 * np.pi))
     # Here we are saying that we need to rescale the order (1,0,0,0) which is
     # the NLO central, in order to obtain the order (2,0,1,0)
     np.testing.assert_allclose(