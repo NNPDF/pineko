11555dfdf55179b77a2ceb1378bcab9caeec32e8
Andrea Barontini <andreab1997@yahoo.it>
2023-05-08 16:39:39 +0200

    Fix all the tests

--- benchmarks/bench_checks.py
+++ benchmarks/bench_checks.py
@@ -24,8 +24,8 @@ def benchmark_check_grid_and_eko_compatible(test_files, tmp_path):
         test_files / "data/ekos/400/HERA_NC_225GEV_EP_SIGMARED.tar"
     ) as ekoop:
         with pytest.raises(ValueError):
-            pineko.check.check_grid_and_eko_compatible(wrong_grid, ekoop, 1.0, 10, 10)
-        pineko.check.check_grid_and_eko_compatible(grid, ekoop, 1.0, 10, 10)
+            pineko.check.check_grid_and_eko_compatible(wrong_grid, ekoop, 1.0, 3, 3)
+        pineko.check.check_grid_and_eko_compatible(grid, ekoop, 1.0, 3, 3)
         eko.io.manipulate.xgrid_reshape(
             ekoop, targetgrid=eko.interpolation.XGrid([0.0001, 0.001, 0.1, 0.5, 1.0])
         )
