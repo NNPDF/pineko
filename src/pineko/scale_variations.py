82c1ed9bfabe5586e1b20d80fe6a3e3b77d4c305
Andrea Barontini <andreab1997@yahoo.it>
2023-05-12 10:21:42 +0200

    Remove a bunch of raw

--- src/pineko/scale_variations.py
+++ src/pineko/scale_variations.py
@@ -89,20 +89,18 @@ def create_svonly(grid, order, new_order, scalefactor):
     grid_orders = [order.as_tuple() for order in grid.orders()]
     order_index = grid_orders.index(order)
     for lumi_index in range(len(new_grid.lumi())):
-        for bin_index in range(grid.raw.bins()):
+        for bin_index in range(grid.bins()):
             extracted_subgrid = grid.subgrid(order_index, bin_index, lumi_index)
             extracted_subgrid.scale(scalefactor)
             # Set this subgrid inside the new grid
             new_grid.set_subgrid(0, bin_index, lumi_index, extracted_subgrid)
     # Fixing bin_limits and normalizations
-    bin_dimension = grid.raw.bin_dimensions()
+    bin_dimension = grid.bin_dimensions()
     limits = []
-    for num_bin in range(grid.raw.bins()):
+    for num_bin in range(grid.bins()):
         for dim in range(bin_dimension):
-            limits.append(
-                (grid.raw.bin_left(dim)[num_bin], grid.raw.bin_right(dim)[num_bin])
-            )
-    norma = grid.raw.bin_normalizations()
+            limits.append((grid.bin_left(dim)[num_bin], grid.bin_right(dim)[num_bin]))
+    norma = grid.bin_normalizations()
     remap_obj = pineappl.bin.BinRemapper(norma, limits)
     new_grid.set_remapper(remap_obj)
     return new_grid
@@ -146,14 +144,14 @@ def write_grids(gridpath, grid_list):
         if len(grid_list[order]) > 1:
             for grid in grid_list[order][1:]:
                 tmp_path = gridpath.parent / ("tmp" + final_part)
-                grid.raw.write_lz4(tmp_path)
-                grid_list[order][0].raw.merge_from_file(tmp_path)
+                grid.write_lz4(tmp_path)
+                grid_list[order][0].merge_from_file(tmp_path)
                 tmp_path.unlink()
         new_grid_path = gridpath.parent / (
             base_name + "_" + str(order[2]) + final_part
         )  # order[2] is the ren_sv order
         grid_paths.append(new_grid_path)
-        grid_list[order][0].raw.write_lz4(new_grid_path)
+        grid_list[order][0].write_lz4(new_grid_path)
     return grid_paths
 
 
@@ -169,15 +167,15 @@ def merge_grids(
     if order_exists:
         grid = construct_and_dump_order_exists_grid(grid, list(nec_orders.keys())[0])
     for grid_path in grid_list_path:
-        grid.raw.merge_from_file(grid_path)
+        grid.merge_from_file(grid_path)
         grid_path.unlink()
-    grid.raw.write_lz4(target_path)
+    grid.write_lz4(target_path)
 
 
 def construct_and_dump_order_exists_grid(ori_grid, to_construct_order):
     """Remove the order that has to be substituted from the grid."""
-    bin_limits = [float(bin) for bin in range(ori_grid.raw.bins() + 1)]
-    lumi_grid = [pineappl.lumi.LumiEntry(mylum) for mylum in ori_grid.raw.lumi()]
+    bin_limits = [float(bin) for bin in range(ori_grid.bins() + 1)]
+    lumi_grid = [pineappl.lumi.LumiEntry(mylum) for mylum in ori_grid.lumi()]
     subgrid_params = pineappl.subgrid.SubgridParams()
     ori_grid_orders = [order.as_tuple() for order in ori_grid.orders()]
     new_orders = [
@@ -191,7 +189,7 @@ def construct_and_dump_order_exists_grid(ori_grid, to_construct_order):
     orders_indeces = [ori_grid_orders.index(order.as_tuple()) for order in new_orders]
     for order_index in orders_indeces:
         for lumi_index in range(len(lumi_grid)):
-            for bin_index in range(ori_grid.raw.bins()):
+            for bin_index in range(ori_grid.bins()):
                 extr_subgrid = ori_grid.subgrid(order_index, bin_index, lumi_index)
                 new_grid.set_subgrid(
                     orders_indeces.index(order_index),
@@ -199,17 +197,17 @@ def construct_and_dump_order_exists_grid(ori_grid, to_construct_order):
                     lumi_index,
                     extr_subgrid,
                 )
-    bin_dimension = ori_grid.raw.bin_dimensions()
+    bin_dimension = ori_grid.bin_dimensions()
     limits = []
-    for num_bin in range(ori_grid.raw.bins()):
+    for num_bin in range(ori_grid.bins()):
         for dim in range(bin_dimension):
             limits.append(
                 (
-                    ori_grid.raw.bin_left(dim)[num_bin],
-                    ori_grid.raw.bin_right(dim)[num_bin],
+                    ori_grid.bin_left(dim)[num_bin],
+                    ori_grid.bin_right(dim)[num_bin],
                 )
             )
-    norma = ori_grid.raw.bin_normalizations()
+    norma = ori_grid.bin_normalizations()
     remap_obj = pineappl.bin.BinRemapper(norma, limits)
     new_grid.set_remapper(remap_obj)
     new_grid.set_key_value("initial_state_2", ori_grid.key_values()["initial_state_2"])
