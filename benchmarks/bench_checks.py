f7bd8c99a581c774d884de408dbd381316a7492e
andreab1997 <andreab1997@yahoo.it>
2023-05-22 16:14:34 +0200

    Fix bench_checks

--- benchmarks/bench_checks.py
+++ benchmarks/bench_checks.py
@@ -16,10 +16,6 @@ def benchmark_check_grid_and_eko_compatible(test_files, tmp_path):
     wrong_grid = pineappl.grid.Grid.read(
         test_files / "data/grids/208/NUTEV_CC_NU_FE_SIGMARED.pineappl.lz4"
     )
-    with pytest.warns(UserWarning, match="alphas"):
-        eko.io.legacy.load_tar(
-            test_files / "data/ekos/208/HERA_CC_318GEV_EM_SIGMARED.tar", oppath
-        )
     with eko.EKO.edit(
         test_files / "data/ekos/400/HERA_NC_225GEV_EP_SIGMARED.tar"
     ) as ekoop:
