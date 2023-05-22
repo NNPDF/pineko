38c2d2bcae9e880e0408ce1b76541350f857783b
andreab1997 <andreab1997@yahoo.it>
2023-05-22 16:11:33 +0200

    Fix bench_evolve

--- benchmarks/bench_evolve.py
+++ benchmarks/bench_evolve.py
@@ -26,7 +26,6 @@ def benchmark_write_operator_card_from_file(tmp_path, test_files, test_configs):
     myopcard = yaml.safe_load(target_path.read_text(encoding="utf-8"))
     # Check if it contains all the information for eko
     assert np.allclose(myopcard["xgrid"], x_grid)
-    assert np.allclose(myopcard["pids"], eko.basis_rotation.flavor_basis_pids)
 
     wrong_pine_path = test_files / "data/grids/208/HERA_CC_318GEV_EM_wrong.pineappl.lz4"
     with pytest.raises(FileNotFoundError):
