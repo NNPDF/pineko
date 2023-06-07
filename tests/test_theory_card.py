e40c55208618380d63cd1c1e8f63c0473d2a38ec
andreab1997 <andreab1997@yahoo.it>
2023-06-07 12:21:21 +0200

    Add test for theorycard

---
+++ tests/test_theory_card.py
@@ -0,0 +1,15 @@
+import pineko.theory_card
+
+
+def test_construct_assumptions():
+    fake_t_card = {
+        "Q0": 1.65,
+        "kcThr": 1.0,
+        "kbThr": 1.0,
+        "ktThr": 1.0,
+        "mc": 2.0,
+        "mb": 3.0,
+        "mt": 50.0,
+        "IC": True,
+    }
+    assert pineko.theory_card.construct_assumptions(fake_t_card) == "Nf4Sym"
