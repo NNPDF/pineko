238f8802cd6900967773e1dee6fb5c9ccb6d6446
andreab1997 <andreab1997@yahoo.it>
2023-09-21 10:39:33 +0200

    Fix bench_fonll

--- benchmarks/bench_fonll.py
+++ benchmarks/bench_fonll.py
@@ -4,8 +4,10 @@ import yaml
 import pineko
 
 
-@pytest.mark.parametrize("theoryid", [400, 208])
-def benchmark_produce_fonll_tcards(tmp_path, test_files, test_configs, theoryid):
+@pytest.mark.parametrize("theoryid,is_mixed", [(400, False), (208, True)])
+def benchmark_produce_fonll_tcards(
+    tmp_path, test_files, test_configs, theoryid, is_mixed
+):
     tcard = pineko.theory_card.load(theoryid)
     tcard_paths_list = pineko.fonll.produce_fonll_tcards(tcard, tmp_path, theoryid)
     # Check they are correct
@@ -15,10 +17,10 @@ def benchmark_produce_fonll_tcards(tmp_path, test_files, test_configs, theoryid)
             theorycards.append(yaml.safe_load(f))
     for simFONLL_tcard, fns, nfff, po, part in zip(
         theorycards,
-        pineko.fonll.FNS_LIST,
-        pineko.fonll.NFFF_LIST,
-        pineko.fonll.produce_ptos(tcard["FNS"]),
-        pineko.fonll.PARTS_LIST,
+        pineko.fonll.MIXED_FNS_LIST if is_mixed else pineko.fonll.FNS_LIST,
+        pineko.fonll.MIXED_NFFF_LIST if is_mixed else pineko.fonll.NFFF_LIST,
+        pineko.fonll.produce_ptos(tcard["FNS"], is_mixed),
+        pineko.fonll.MIXED_PARTS_LIST if is_mixed else pineko.fonll.PARTS_LIST,
     ):
         assert simFONLL_tcard["FNS"] == fns
         assert simFONLL_tcard["NfFF"] == nfff
