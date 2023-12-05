91aec0c09c31c53fc6a256fb2a3edc05c39e1c19
andreab1997 <andreab1997@yahoo.it>
2023-12-05 11:40:51 +0100

    Correct test_fonll

--- tests/test_fonll.py
+++ tests/test_fonll.py
@@ -7,11 +7,9 @@ def test_FONLLInfo():
     full_list = [
         "ffns3.pineappl.lz4",
         "ffn03.pineappl.lz4",
-        "ffns4.pineappl.lz4",
         "ffns4til.pineappl.lz4",
         "ffns4bar.pineappl.lz4",
         "ffn04.pineappl.lz4",
-        "ffns5.pineappl.lz4",
         "ffns5til.pineappl.lz4",
         "ffns5bar.pineappl.lz4",
     ]
@@ -24,20 +22,16 @@ def test_FONLLInfo():
         full_list[4],
         None,
         None,
-        None,
-        full_list[8],
     )
     partialfonll_fake_info = pineko.fonll.FONLLInfo(
-        full_list[0], full_list[1], None, full_list[3], None, None, None, None, None
+        full_list[0], full_list[1], full_list[2], full_list[3], None, None, None
     )
     name_list = [
         "ffns3",
         "ffn03",
-        "ffns4",
         "ffns4til",
         "ffns4bar",
         "ffn04",
-        "ffns5",
         "ffns5til",
         "ffns5bar",
     ]
@@ -47,14 +41,12 @@ def test_FONLLInfo():
     assert wrongfonll_fake_info.fk_paths == {
         name: pathlib.Path(fk)
         for name, fk in zip(
-            name_list[:2] + name_list[3:5] + [name_list[-1]],
-            full_list[:2] + full_list[3:5] + [full_list[-1]],
+            name_list[:2] + name_list[3:5],
+            full_list[:2] + full_list[3:5],
         )
     }
     assert partialfonll_fake_info.fk_paths == {
         name: pathlib.Path(fk)
-        for name, fk in zip(
-            name_list[:2] + name_list[3:4], full_list[:2] + full_list[3:4]
-        )
+        for name, fk in zip(name_list[:4], full_list[:4])
         if fk is not None
     }
