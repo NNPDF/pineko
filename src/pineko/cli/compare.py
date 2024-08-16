d68f2be8ceae03c31f7e8d8b44178dfa5716e321
Giacomo Magni <giac.magni@gmail.com>
2024-08-16 12:52:45 +0200

    restore nargs=-1 and add a check

--- src/pineko/cli/compare.py
+++ src/pineko/cli/compare.py
@@ -13,7 +13,7 @@ from ._base import command
 @click.argument("grid_path", type=click.Path(exists=True))
 @click.argument("max_as", type=int)
 @click.argument("max_al", type=int)
-@click.argument("pdfs", type=click.STRING, nargs=2)
+@click.argument("pdfs", type=click.STRING, nargs=-1)
 @click.option("--xir", default=1.0, help="renormalization scale variation")
 @click.option("--xif", default=1.0, help="factorization scale variation")
 def subcommand(fktable_path, grid_path, max_as, max_al, pdfs, xir, xif):
@@ -31,6 +31,11 @@ def subcommand(fktable_path, grid_path, max_as, max_al, pdfs, xir, xif):
     """
     pine = pineappl.grid.Grid.read(grid_path)
     fk = pineappl.fk_table.FkTable.read(fktable_path)
+
+    if len(pdfs) > 2:
+        raise ValueError(
+            "Pineko supports the concolution with maximum 2 different PDF sets."
+        )
     pdf1 = pdfs[0]
     pdf2 = pdfs[1] if len(pdfs) == 2 else None
     # Note that we need to cast to string before printing to avoid ellipsis ...
